02681f8b0c8235a8f6addcb70e8ce02d8f9d81fb
doantrungtinn <doantrungtinn@gmail.com>
2022-06-20 16:08:36 +0700

    commit

---
+++ restapi/src/test/java/Assignmet_TinDoan/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package Assignmet_TinDoan.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
